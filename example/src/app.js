e097b0e2fbed9392e647ee56ed1efcb9a3acebc1
fritz-c <fritz.christopher@redfox.co.jp>
2016-04-05 10:42:55 +0900

    Make app example easier to serve on gh-pages without modification

--- example/src/app.js
+++ example/src/app.js
@@ -3,52 +3,46 @@ var ReactDOM = require('react-dom');
 var Lightbox = require('react-image-lightbox');
 
 var images = [
-    '/images/1.jpg',
-    '/images/2.jpg',
-    '/images/3.jpg',
-    '/images/4.jpg',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
 ];
 
 var thumbs = [
-    '/images/1_thumb.jpg',
-    '/images/2_thumb.jpg',
-    '/images/3_thumb.jpg',
-    '/images/4_thumb.jpg',
+    'images/1_thumb.jpg',
+    'images/2_thumb.jpg',
+    'images/3_thumb.jpg',
+    'images/4_thumb.jpg',
 ];
 
 var titles = [
-    "",
-    (
-        <span>
-            by&nbsp;
-            <a href="http://flickr.com/photos/titrans/">
-                quatre mains
-            </a>
-            &nbsp;(<a href="http://creativecommons.org/licenses/by/2.0/" title="Attribution License">
-                Some rights reserved
-            </a>)
-        </span>
-    ),
-    (
-        <span>
-            by&nbsp;
-            <a href="http://flickr.com/photos/lachlanrogers/">latch.r</a>
-            &nbsp;(<a href="http://creativecommons.org/licenses/by-sa/2.0/" title="Attribution-ShareAlike License">
-                Some rights reserved
-            </a>)
-        </span>
-    ),
-    (
-        <span>
-            by&nbsp;
-            <a href="http://flickr.com/photos/fazen/">
-                fazen
-            </a>
-            &nbsp;(<a href="http://creativecommons.org/licenses/by/2.0/" title="Attribution License">
-                Some rights reserved
-            </a>)
-        </span>
-    ),
+    '',
+    (<span>
+        by&nbsp;
+        <a href="http://flickr.com/photos/titrans/">
+            quatre mains
+        </a>
+        &nbsp;(<a href="http://creativecommons.org/licenses/by/2.0/" title="Attribution License">
+            Some rights reserved
+        </a>)
+    </span>),
+    (<span>
+        by&nbsp;
+        <a href="http://flickr.com/photos/lachlanrogers/">latch.r</a>
+        &nbsp;(<a href="http://creativecommons.org/licenses/by-sa/2.0/" title="Attribution-ShareAlike License">
+            Some rights reserved
+        </a>)
+    </span>),
+    (<span>
+        by&nbsp;
+        <a href="http://flickr.com/photos/fazen/">
+            fazen
+        </a>
+        &nbsp;(<a href="http://creativecommons.org/licenses/by/2.0/" title="Attribution License">
+            Some rights reserved
+        </a>)
+    </span>),
 ];
 var App = React.createClass({
     getInitialState: function() {
