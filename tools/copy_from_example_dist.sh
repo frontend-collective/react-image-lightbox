d5d2747e36a3f080288d8b8029ac90fc5b6a1dd5
fritz-c <fritz-c@users.noreply.github.com>
2016-07-15 14:22:55 +0900

    Updates

--- tools/copy_from_example_dist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Move example/dist files to their spots for the example
-#
-# Usage: ./copy_from_example_dist.sh
-BASE_DIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
-cp "${BASE_DIR}/example/dist/"{app.css,app.js,bundle.js,common.js,index.html} "${BASE_DIR}/"
