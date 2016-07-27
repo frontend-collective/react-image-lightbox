3df056766bba4d2e902b96485156505cc65dadb6
fritz-c <fritz-c@users.noreply.github.com>
2016-07-27 13:19:21 +0900

    Fix mobile (safari) visual bug
    The edges of the lightbox were getting cut off due to the
    react-modal style settings. For some reason this didn't come
    up on desktop browsers.

--- src/react-image-lightbox.js
+++ src/react-image-lightbox.js
@@ -855,6 +855,10 @@ class ReactImageLightbox extends Component {
                 backgroundColor: 'transparent',
                 border:          'none',
                 borderRadius:    0,
+                top:             0,
+                left:            0,
+                right:           0,
+                bottom:          0,
             },
         };
 
