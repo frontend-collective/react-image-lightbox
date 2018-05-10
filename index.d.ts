e3f35428edf4d8736e40464760a16bc166885110
Efremov Alexey <lexich121@gmail.com>
2018-05-10 10:31:46 +0300

    Add: typescript declarations files

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+export interface ILightBoxProps {
+    mainSrc: string;
+    nextSrc?: string;
+    prevSrc?: string;
+    mainSrcThumbnail?: string;
+    prevSrcThumbnail?: string;
+    nextSrcThumbnail?: string;
+    onCloseRequest(): void;
+    onMovePrevRequest?(): void;
+    onMoveNextRequest?(): void;
+    onImageLoad?(): void;
+    onImageLoadError?(): void;
+    imageLoadErrorMessage?: React.ReactNode;
+    onAfterOpen?(): void;
+    discourageDownloads?: boolean;
+    animationDisabled?: boolean;
+    animationOnKeyInput?: boolean;
+    animationDuration?: number;
+    keyRepeatLimit?: number;
+    keyRepeatKeyupBonus?: number;
+    imageTitle?: string;
+    imageCaption?: string;
+    imageCrossOrigin?: string;
+    toolbarButtons?: React.ReactNode[];
+    reactModalStyle?: any;
+    reactModalProps?: any;
+    imagePadding?: number;
+    clickOutsideToClose?: boolean;
+    enableZoom?: boolean;
+    wrapperClassName?: string;
+    nextLabel?: string;
+    prevLabel?: string;
+    zoomInLabel?: string;
+    zoomOutLabel?: string;
+    closeLabel?: string;
+}
+
+export default class Lightbox extends React.Component<ILightBoxProps, never> { }
