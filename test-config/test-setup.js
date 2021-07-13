082ffbdd4b11e71f99c01c985472fb51cf2e590e
Bruno Alla <browniebroke@users.noreply.github.com>
2021-07-13 12:35:45 +0200

    fix: update peer dependencies to allow React 17
    The main blocking part was removed in #227.
    It should be fine to allow React 17 now.
    
    Fix #303
    Fix #335
    Fix #196

--- test-config/test-setup.js
+++ test-config/test-setup.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import 'regenerator-runtime/runtime';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
